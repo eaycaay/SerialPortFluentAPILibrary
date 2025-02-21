ba9605c5c08f65c4523e44de113db9400a8712c9
eaycaay <aycaayvaz@gmail.com>
2025-02-21 23:40:03 +0300

    Add files via upload

---
+++ SerialPortDeneme/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SerialPortFluentAPILib;
+using Superpower;
+
+namespace SerialPortDeneme
+{
+    public partial class Form1: Form
+    {
+        SerialPortLib serialPortLib;
+        public Form1()
+        {
+            InitializeComponent();
+            serialPortLib = SerialPortLib.GetInstance;
+        }
+        private Parity parity;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialPortLib.SetPortName(textBox1.Text).SetDataBits(Int32.Parse(textBox2.Text)).SetDataBits(Int32.Parse(textBox3.Text)).SetBaudRate(Int32.Parse(textBox4.Text)).SetBytesToRead(parity);
+        }
+    }
+}
---
+++ SerialPortFluentAPILib/SerialPortLib.cs
@@ -0,0 +1,47 @@
+﻿using System.IO.Ports;
+
+namespace SerialPortFluentAPILib
+{
+    public class SerialPortLib
+    {
+        public static SerialPortLib serialPortLib = new SerialPortLib();
+        public static SerialPortLib GetInstance
+        {
+            get
+            {
+                return serialPortLib;
+            }        
+        }
+        SerialPort serialPort = new SerialPort();
+
+
+        public SerialPortLib SetBaudRate(int baud)
+        {
+
+            this.serialPort.BaudRate = baud;
+            return serialPortLib;
+        }
+        public SerialPortLib SetDataBits(int dataBits)
+        {
+
+            this.serialPort.DataBits = dataBits;
+            return serialPortLib;
+        }
+        public SerialPortLib SetBytesToRead(Parity parity)
+        {
+
+            this.serialPort.Parity = parity;
+            return serialPortLib;
+        }
+        public SerialPortLib SetPortName(String portName)
+        {
+
+            this.serialPort.PortName= portName;
+            return serialPortLib;
+        }
+       
+
+
+
+    }
+}
